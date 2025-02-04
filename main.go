d22a8ebce6890e1f267dd2d23c413374d7cbc025
pongfcnkl <24273319+pongfcnkl@users.noreply.github.com>
2025-02-04 23:58:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
+
 import (
 	"os"
 	"github.com/alist-org/alist/v3/cmd"
 )
 
-
 func main() {
-	os.Args = append(os.Args, "server")
+	// 设置第一个命令的参数
+	os.Args = []string{"", "admin", "set", "1"}
+	cmd.Execute()
+
+	// 设置第二个命令的参数
+	os.Args = []string{"", "server"}
 	cmd.Execute()
 }
