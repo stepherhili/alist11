26f71b6e62e980a1fd3ab0152b0176e70c2497b8
pongfcnkl <24273319+pongfcnkl@users.noreply.github.com>
2025-02-04 20:24:21 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
-package os
-import "github.com/alist-org/alist/v3/cmd"
+import (
+	"os"
+	"github.com/alist-org/alist/v3/cmd"
+)
+
 
 func main() {
 	os.Args = append(os.Args, "--server")
