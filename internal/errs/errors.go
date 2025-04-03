b1ea8b5db5c990ac757a1d2b7f38bb5d5ca969b2
pongfcnkl <24273319+pongfcnkl@users.noreply.github.com>
2025-04-03 23:37:23 +0800

    Add files via upload

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -12,8 +12,7 @@ var (
 	NotSupport   = errors.New("not support")
 	RelativePath = errors.New("access using relative path is not allowed")
 
-	MoveBetweenTwoStorages = errors.New("can't move files between two storages, try to copy")
-	UploadNotSupported     = errors.New("upload not supported")
+	UploadNotSupported = errors.New("upload not supported")
 
 	MetaNotFound     = errors.New("meta not found")
 	StorageNotFound  = errors.New("storage not found")
