db083dbf1a77af7495c50cfc636ac4eda99f8951
pongfcnkl <24273319+pongfcnkl@users.noreply.github.com>
2025-08-22 13:18:26 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ FetchWebDev() {
 }
 
 FetchWebRelease() {
-  curl -L https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+  curl -L https://github.com/pongfcnkl/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
   tar -zxvf dist.tar.gz
   rm -rf public/dist
   mv -f dist public
